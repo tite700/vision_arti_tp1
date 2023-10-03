5a9bb33a9b0ec6662da4487c1eb912cef11da36b
Baptiste <tite7001@gmail.com>
2023-10-03 10:59:36 -0400

    Rajout images seuillees

--- tp1.py
+++ tp1.py
@@ -1,4 +1,3 @@
-# Import required packages:
 import cv2
 import os
 import numpy as np
@@ -26,51 +25,35 @@ for dossier in dossiers_source:
             image_filtree = cv2.GaussianBlur(image_corrigee, (5, 5), 0)
             image_filtree = image_filtree.astype(np.uint8)
             
-            seuil = 100  
+            seuil = 50
             _, image_seuillee = cv2.threshold(image_filtree, seuil, 255, cv2.THRESH_BINARY)
             _, image_otsu = cv2.threshold(image_filtree, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-            # Définir le nom du fichier de sortie
             nom_fichier_sortie = os.path.splitext(fichier)[0]
 
-            # Enregistrer les images seuillées dans le dossier cible
             chemin_fichier_sortie_seuil = os.path.join(chemin_dossier_cible, nom_fichier_sortie + "_seuil.jpg")
             chemin_fichier_sortie_otsu = os.path.join(chemin_dossier_cible, nom_fichier_sortie + "_otsu.jpg")
-
-            cv2.imwrite(chemin_fichier_sortie_seuil, image_seuillee)
             cv2.imwrite(chemin_fichier_sortie_otsu, image_otsu)
 
-            # Charger l'image de référence en niveaux de gris (CV_8UC1)
             image_reference = cv2.imread(os.path.join(repertoire_source, dossier, "Reference.JPG"), 0)
-
-            # Convertir l'image en niveaux de gris en une image en noir et blanc (RVB)
             image_reference_rgb = cv2.cvtColor(image_reference, cv2.COLOR_GRAY2BGR)
 
-            # Afficher l'image de référence
             cv2.imshow("Image de Référence", image_reference_rgb)
-
-            # Charger l'image avec les changements détectés
             image_changements = cv2.imread(chemin_fichier_sortie_otsu,0)
 
-            # Afficher l'image de référence et l'image avec les changements détectés
             image_reference = cv2.resize(image_reference, (0, 0), fx=0.1, fy=0.1)
             cv2.imshow("Image de Référence", image_reference)
+            cv2.imshow("Image seuillée", image_seuillee)
             cv2.imshow("Image de base", image)
             cv2.imshow("Image avec Changements Détectés", image_changements)
+            x_offset = 10  
+            y_offset = 10  
 
-            # Définir les coordonnées de positionnement des fenêtres
-            x_offset = 10  # Décalage horizontal
-            y_offset = 10  # Décalage vertical
-
-            # Déplacer les fenêtres à leurs positions respectives
             cv2.moveWindow("Image de Référence", x_offset, y_offset)
             cv2.moveWindow("Image de base", x_offset * 2 + image_reference.shape[1], y_offset)
             cv2.moveWindow("Image avec Changements Détectés", x_offset * 3 + 2 * image_reference.shape[1], y_offset)
+            cv2.moveWindow("Image seuillée", x_offset * 3 + 2 * image_reference.shape[1], y_offset * 2 + image_reference.shape[0]) 
 
-            cv2.waitKey(0)  # Attendre une touche pour fermer la fenêtre
+            cv2.waitKey(0)  
 
-
-            cv2.waitKey(0)  # Attendre une touche pour fermer la fenêtre
-
-# Fermer toutes les fenêtres OpenCV (si vous en avez ouvert)
 cv2.destroyAllWindows()
